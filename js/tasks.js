2942f9d82618708e13bb3c6daf4c82d2a6088713
antagonist13 <1703shakayaroslaw@gmail.com>
2024-03-05 17:11:52 +0200

    1

--- js/tasks.js
+++ js/tasks.js
@@ -59,3 +59,18 @@
 
 
 //===============================================================================
+
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+String.prototype.toJadenCase = function () {
+   return this.split(' ').map( item => item[0].toUpperCase() + item.slice(1)).join(' ')
+};
+
+var str = "How can mirrors be real if our eyes aren't real";
+console.log(str.toJadenCase());
+
+
+// String.prototype.toJadenCase = function() {
+//   return this.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ')
+// };
